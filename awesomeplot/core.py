ab0967a17151cc789fe9a3b9b6625accf6f675ce
Paul Schultz <pschultz@pik-potsdam.de>
2017-12-20 13:48:53 +0100

    savefig adjusted due to different keyword args in matplotlib

--- awesomeplot/core.py
+++ awesomeplot/core.py
@@ -756,13 +756,13 @@ class Plot(object):
 
     def save(self, fnames, fig = None):
         if fig:
-            fig.savefig(filename=fnames + '.' + self.figure_format, bbox_inches='tight', transparent=self.transparent)
+            fig.savefig(fnames + '.' + self.figure_format, bbox_inches='tight', transparent=self.transparent)
             self.clear(fig)
         else:
             assert len(fnames) == len(self.figures)
             for i, fig in enumerate(self.figures):
                 print "save:", fnames[i] + '.' + self.figure_format
-                fig.savefig(filename=fnames[i] + '.' + self.figure_format, bbox_inches='tight', transparent=self.transparent)
+                fig.savefig(fnames[i] + '.' + self.figure_format, bbox_inches='tight', transparent=self.transparent)
                 pyplot.close(fig)
             for i, fig in enumerate(self.figures):
                 self.figures.remove(fig)
